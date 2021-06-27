4a157d3bf8c5a0b56d1a386bba7a1054d3d757d2
Angelo <angelomilienjoseph@gmail.com>
2021-06-27 18:55:24 -0400

    done

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,50 @@
+class SongsController < ApplicationController
+
+	def index
+		@songs = Song.all
+	end
+
+	def show
+		@song = Song.find(params[:id])
+	end
+
+	def new
+		@song = Song.new
+	end
+
+	def create
+	  @song = Song.new(song_params)
+	  @song.save
+	  redirect_to song_path(@song)
+	end
+
+	def edit
+		@song = Song.find(params[:id])
+	end
+
+	def update
+	  @song = Song.find(params[:id])
+	  @song.update(song_params)
+	  redirect_to song_path(@song)
+	end
+
+	private
+
+		def song_params
+			params.require(:song).permit(:name, :artist_id, :genre_id)
+		end
+end
+
+
+# def update
+#     @student = Student.find(params[:id])
+#     @student.update(students_params(:first_name, :last_name))
+#     redirect_to student_path(@student)
+#   end
+
+#   private
+
+
+#   def students_params(*args)
+#     params.require(:student).permit(*args)
+#   end
